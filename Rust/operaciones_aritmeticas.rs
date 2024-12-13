403815d52ac7541787bd18cb266a2f86f713a4b5
Jean Karin <jean.karin@mail.ru>
2024-12-13 16:36:55 +0100

    añadimos operadores aritméticos en rust.

---
+++ Rust/operaciones_aritmeticas.rs
@@ -0,0 +1,24 @@
+fn main() {
+    // addition
+    let sum = 5 + 10;
+    println!("Suma {sum}");
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+    println!("Resta {difference}");
+
+    // multiplication
+    let product = 4 * 30;
+    println!("Multiplicación {product}");
+
+    // division
+    let quotient = 56.7 / 32.2;
+    println!("Coeficiente división {quotient}");
+    
+    let truncated = -5 / 3; // Results in -1
+    println!("Truncar el resultado {truncated}");
+
+    // remainder
+    let remainder = 43 % 5;
+    println!("Resultado división {remainder}");
+}
